c843b6b3f9f5e481db5f69dc0aa766d65dae01ae
jishnuc <jishnuchandru009@gmail.com>
2025-05-20 14:33:57 +0530

    order crud complete

--- order/crud.py
+++ order/crud.py
@@ -11,14 +11,27 @@ async def get_user_orders(db: AsyncSession, user_id: int):
         select(Order)
         .filter_by(user_id = user_id)
         .options(
-            joinedload(Order.volume).joinedload(MangaVolume.images)
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.images),
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.publisher),
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.manga)
         )
     )
     return result.unique().scalars().all()
 
 async def place_order(db: AsyncSession, user_id: int, order_data: OrderCreate):
     # Validate volume exists
-    result = await db.execute(select(MangaVolume).filter_by(id=order_data.volume_id))
+    result = await db.execute(
+        select(MangaVolume)
+        .options(
+            joinedload(MangaVolume.images),
+            joinedload(MangaVolume.publisher),
+            joinedload(MangaVolume.manga)
+        )
+        .filter_by(id=order_data.volume_id)
+    )
     volume = result.scalars().first()
     if not volume:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Volume not found")
@@ -32,4 +45,18 @@ async def place_order(db: AsyncSession, user_id: int, order_data: OrderCreate):
     db.add(order)
     await db.commit()
     await db.refresh(order)
-    return order
+
+    result = await db.execute(
+        select(Order)
+        .options(
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.images),
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.publisher),
+            joinedload(Order.volume)
+            .joinedload(MangaVolume.manga)
+        )
+    )
+    order_item = result.unique().scalars().first()
+
+    return order_item
