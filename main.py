a1696f103250b9d8bb9f4aaa6a3f8c33909c22d1
jishnuc <jishnuchandru009@gmail.com>
2025-05-20 14:10:28 +0530

    main.py complete

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from database.database import init_db, close_db
 from auth.routes import router as auth_router
 from manga.routes import router as manga_router
 from cart.routes import router as cart_router
 from order.routes import router as order_router
+
+load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,6 +20,14 @@ async def lifespan(app: FastAPI):
     await close_db()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.getenv("allow_origins"),
+    allow_credentials=os.getenv("allow_credentials"),
+    allow_methods=os.getenv("allow_methods"),
+    allow_headers=os.getenv("allow_headers"),
+)
 
 app.include_router(auth_router)
 app.include_router(manga_router)
